332f3206d4a5542672397b4379cdf25c89b0a514
ciromoura <ciro.dgm@gmail.com>
2022-08-10 22:43:06 -0300

    add homepage

--- Socket.io/vite-project/src/main.jsx
+++ Socket.io/vite-project/src/main.jsx
@@ -4,13 +4,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+import Home from "./components/Home";
 import Game from "./components/Game";
 import Winner from "./components/Winner";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />}></Route>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/app" element={<App />}></Route>
       <Route path="/game" element={<Game />}></Route>
       <Route path="/game-result" element={<Winner />}></Route>
     </Routes>
