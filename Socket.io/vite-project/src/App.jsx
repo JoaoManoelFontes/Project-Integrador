2c6394a67e909a0c09cc741f202ee1a2a2ab68a6
JoaoManoelFontes <jmanufontesc@gmail.com>
2022-09-06 22:00:33 -0300

    rename vite-project to front-end

--- Socket.io/vite-project/src/App.jsx
+++
@@ -1,5 +0,0 @@
-import { MappedRoutes } from "./routes";
-
-export default function App() {
-  return <MappedRoutes />;
-}
--- Socket.io/vite-project/src/services/client.jsx
+++
@@ -1,3 +0,0 @@
-import io from "socket.io-client";
-const ENDPOINT = "http://localhost:8888";
-export default io(ENDPOINT);
