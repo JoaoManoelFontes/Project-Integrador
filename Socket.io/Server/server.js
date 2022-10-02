a48d889bf6b25409aa7a4558e01405822c3bc052
JoaoManoelFontes <jmanufontesc@gmail.com>
2022-10-02 15:59:34 -0300

    add new status funcionality

--- Socket.io/Server/server.js
+++ Socket.io/Server/server.js
@@ -9,121 +9,122 @@ let players = {};
 
 //?Socket events
 io.on("connection", (socket) => {
-    console.log("New client connected: " + socket.id);
-    // socket.emit("newRoom", rooms);
-    socket.emit("myId", socket.id);
+  console.log("New client connected: " + socket.id);
+  // socket.emit("newRoom", rooms);
+  socket.emit("myId", socket.id);
 
-    //?Função batalhar - entrando em uma sala aleatória
-    socket.on("battle", () => {
-        if (rooms.length == 0) {
-            socket.emit(
-                "joinRoomError",
-                "Não existe nenhuma sala disponível no momento, crie uma para continuar"
-            );
-        } else {
-            rooms.map((room, index) => {
-                if (clients[room] == 1) {
-                    clients[room]++;
-                    socket.join(room);
-                    socket.room = room;
-                    socket.emit("roomJoined", {
-                        room: room,
-                        status: "player 2 joined, waiting to start the game",
-                    });
-                    io.to(room).emit(
-                        "status",
-                        "player 2 joined, waiting to start the game"
-                    );
-                    io.to(room).emit("playerId", socket.id);
-                    io.to(room).emit(
-                        "cardStatus",
-                        `ID: ${socket.id.substr(0, 8)}... <br />Jogador encontrado`
-                    );
-                } else if (clients[room] == 2 && index == rooms.length - 1) {
-                    socket.emit(
-                        "joinRoomError",
-                        "Não existe nenhuma sala disponível no momento, crie uma para continuar"
-                    );
-                }
-            });
-        }
-    });
-
-    //?Criando uma sala
-    socket.on("generateRoom", () => {
-        socket.join(socket.id);
-        socket.room = socket.id;
-        clients[socket.room] = 1;
-        socket.emit("roomGenerated", {
-            room: socket.room,
-            status: "waiting for player 2",
-        });
-        rooms.push(socket.room);
-    });
-
-    //?Entrando numa sala nova
-    socket.on("joinRoom", (room) => {
+  //?Função batalhar - entrando em uma sala aleatória
+  socket.on("battle", () => {
+    if (rooms.length == 0) {
+      socket.emit(
+        "joinRoomError",
+        "Não existe nenhuma sala disponível no momento, crie uma para continuar"
+      );
+    } else {
+      rooms.map((room, index) => {
         if (clients[room] == 1) {
-            clients[room]++;
-            socket.room = room;
-            socket.join(room);
-            socket.emit("roomJoined", {
-                room: room,
-                status: "player 2 joined, waiting to start the game",
-            });
-            io.to(room).emit("status", "player 2 joined, waiting to start the game");
-            io.to(room).emit("playerId", socket.id);
-            io.to(room).emit(
-                "cardStatus",
-                `ID: ${socket.id.substr(0, 8)}... <br />Jogador encontrado`
-            );
-        } else {
-            socket.emit(
-                "joinRoomError",
-                "This room has already full or does not exists! Generate a room to continue"
-            );
+          clients[room]++;
+          socket.join(room);
+          socket.room = room;
+          socket.emit("roomJoined", {
+            room: room,
+            status: "player 2 joined, waiting to start the game",
+          });
+          io.to(room).emit(
+            "status",
+            "player 2 joined, waiting to start the game"
+          );
+          io.to(room).emit("playerId", socket.id);
+          io.to(room).emit(
+            "cardStatus",
+            `ID: ${socket.id.substr(0, 8)}... <br />Jogador encontrado`
+          );
+        } else if (clients[room] == 2 && index == rooms.length - 1) {
+          socket.emit(
+            "joinRoomError",
+            "Não existe nenhuma sala disponível no momento, crie uma para continuar"
+          );
         }
-    });
+      });
+    }
+  });
 
-    //?Escolhendo a carta para batalhar
-    socket.on("sendCard", ({ playerCard, room, id }) => {
-        if (players[room] == undefined) {
-            players[room] = { player1: { playerCard, id }, player2: undefined };
-            io.to(room).emit("status", socket.id + " has already choices his card");
-            io.to(room).emit("senderId", socket.id);
-        } else if (players[room].player1.id == socket.id) {
-            socket.emit("status", "You only can pick a card 1 time ");
-        } else if (players[room].player2 == undefined) {
-            players[room].player2 = { playerCard, id };
-            io.to(room).emit("status", "Players has already choices his card");
-            (async() => {
-                const result = await winner(players[room]);
-                io.to(room).emit("status", result.status);
-                io.to(room).emit("winner", {
-                    winner: result.winner,
-                    cardId: result.cardId,
-                });
-            })();
-        }
+  //?Criando uma sala
+  socket.on("generateRoom", () => {
+    socket.join(socket.id);
+    socket.room = socket.id;
+    clients[socket.room] = 1;
+    socket.emit("roomGenerated", {
+      room: socket.room,
+      status: "waiting for player 2",
     });
+    rooms.push(socket.room);
+  });
 
-    //? saindo da conexão com o server
-    socket.on("disconnect", () => {
-        rooms.map((room) => {
-            if (socket.room == room) {
-                io.to(room).emit(
-                    "leavesRoomError",
-                    "Um jogador deixou esta sala, volte para a página inicial e procure outra batalha"
-                );
-            }
+  //?Entrando numa sala nova
+  socket.on("joinRoom", (room) => {
+    if (clients[room] == 1) {
+      clients[room]++;
+      socket.room = room;
+      socket.join(room);
+      socket.emit("roomJoined", {
+        room: room,
+        status: "player 2 joined, waiting to start the game",
+      });
+      io.to(room).emit("status", "player 2 joined, waiting to start the game");
+      io.to(room).emit("playerId", socket.id);
+      io.to(room).emit(
+        "cardStatus",
+        `ID: ${socket.id.substr(0, 8)}... <br />Jogador encontrado`
+      );
+    } else {
+      socket.emit(
+        "joinRoomError",
+        "This room has already full or does not exists! Generate a room to continue"
+      );
+    }
+  });
+
+  //?Escolhendo a carta para batalhar
+  socket.on("sendCard", ({ playerCard, room, id }) => {
+    if (players[room] == undefined) {
+      players[room] = { player1: { playerCard, id }, player2: undefined };
+      io.to(room).emit("status", socket.id + " has already choices his card");
+      io.to(room).emit("senderId", socket.id);
+    } else if (players[room].player1.id == socket.id) {
+      socket.emit("status", "You only can pick a card 1 time ");
+    } else if (players[room].player2 == undefined) {
+      players[room].player2 = { playerCard, id };
+      io.to(room).emit("status", "Players has already choices his card");
+      io.to(room).emit("senderId", socket.id);
+      (async () => {
+        const result = await winner(players[room]);
+        io.to(room).emit("result_status", result.status);
+        io.to(room).emit("winner", {
+          winner: result.winner,
+          cardId: result.cardId,
         });
-        if (socket.room != undefined) {
-            clients[socket.room]--;
-            players[socket.room] = undefined;
-            if (clients[socket.room] == 0) {
-                rooms.splice(socket.room, 1);
-            }
-        }
-        console.log("Client disconnected");
+      })();
+    }
+  });
+
+  //? saindo da conexão com o server
+  socket.on("disconnect", () => {
+    rooms.map((room) => {
+      if (socket.room == room) {
+        io.to(room).emit(
+          "leavesRoomError",
+          "Um jogador deixou esta sala, volte para a página inicial e procure outra batalha"
+        );
+      }
     });
+    if (socket.room != undefined) {
+      clients[socket.room]--;
+      players[socket.room] = undefined;
+      if (clients[socket.room] == 0) {
+        rooms.splice(socket.room, 1);
+      }
+    }
+    console.log("Client disconnected");
+  });
 });
